9fc62dc2a8a222ddd1faca7233bdc42b4668eaf0
Meraj <merajurrahmanniber@gmail.com>
2023-10-14 11:41:27 +0600

    practice class

---
+++ main.js
@@ -0,0 +1,14 @@
+import student from "./student.js";
+
+const myname = "nibir" //variable declare with const keyword
+let id = 1234 //variable declare with let keyword. 
+//if we want to change the value of a variable, we declare it with let keyword 
+const cgpa = "3.50"
+
+console.log("Name: ",myname,"id: ",id,"CGPA: ",cgpa);
+
+
+const s1 = new student("Abc",123,3.45);
+
+console.log(s1);
+
---
+++ student.js
@@ -0,0 +1,10 @@
+export default class student{
+    myname
+    id
+    cgpa
+    constructor(myname,id,cgpa){
+        this.myname = myname;
+        this.id = id;
+        this.cgpa = cgpa;
+    }
+}
